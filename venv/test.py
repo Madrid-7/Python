1e7bf9ecc937a6361bdf28072106db7d46b2d05b
Madrid-7 <783798088@qq.com>
2020-02-01 17:55:13 +0800

    爬虫练习

--- venv/test.py
+++ venv/test.py
@@ -4,17 +4,17 @@ import requests
 import time
 
 def getHTML(url):
-      try:
-            r = requests.get(url)
-            r.raise_for_status()  #如果不是200，引发HTTP返回异常
-            return r
-      except:
-            return "异常产生"
+    try:
+        r = requests.get(url)
+        r.raise_for_status()  #如果不是200，引发HTTP返回异常
+        return r
+    except:
+        return "异常产生"
 
 if __name__=="__main__":
-      start_time=time.perf_counter()
-      url=input("网站是：")
-      for i in range(100):
-            getHTML(url)
-      end_time=time.perf_counter()
-      print("爬100次{}的时间是{}秒".format(url, end_time-start_time))
+    start_time=time.perf_counter()
+    url=input("网站是：")
+    for i in range(100):
+        getHTML(url)
+    end_time=time.perf_counter()
+    print("爬100次{}的时间是{}秒".format(url, end_time-start_time))
